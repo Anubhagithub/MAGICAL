3218c988333a73c2505a4609c87e3bafc749c9d3
Anubha Dey <72375031+Anubhagithub@users.noreply.github.com>
2024-02-21 16:40:02 +0530

    Update magical-combined.R

--- MAGICAL-combined/magical-combined.R
+++ MAGICAL-combined/magical-combined.R
@@ -1,17 +1,25 @@
 ###MAGICAL-COMBINED
-sl = read.csv("/home/nikola/string-full-nocuff/biogrid-sl-pairwise-new-netprop")
+setwd("/home/user/MAGICAL/MAGICAL-combined")
+#sl = read.csv("/home/nikola/string-full-nocuff/biogrid-sl-pairwise-new-netprop")
+sl = read.csv("biogrid-sl-pairwise-new-netprop")
 sl$gi = "SL"
-sv = read.csv("/home/nikola/string-full-nocuff/biogrid-sv-pairwise-new-netprop")
+#sv = read.csv("/home/nikola/string-full-nocuff/biogrid-sv-pairwise-new-netprop")
+sv = read.csv("biogrid-sv-pairwise-new-netprop")
 sv$gi = "SV"
-sl2 = read.csv("/home/nikola/string-full-nocuff/cgidb-sl-pairwise-new-netprop")
+#sl2 = read.csv("/home/nikola/string-full-nocuff/cgidb-sl-pairwise-new-netprop")
+sl2 = read.csv("cgidb-sl-pairwise-new-netprop")
 sl2$gi = "SL"
-sv2 = read.csv("/home/nikola/string-full-nocuff/cgidb-sv-pairwise-new-netprop")
+#sv2 = read.csv("/home/nikola/string-full-nocuff/cgidb-sv-pairwise-new-netprop")
+sv2 = read.csv("cgidb-sv-pairwise-new-netprop")
 sv2$gi = "SV"
-sl3 = read.csv("/home/nikola/string-full-nocuff/sldb-sl-pairwise-new-netprop")
+#sl3 = read.csv("/home/nikola/string-full-nocuff/sldb-sl-pairwise-new-netprop")
+sl3 = read.csv("sldb-sl-pairwise-new-netprop")
 sl3$gi = "SL"
-sv3 = read.csv("/home/nikola/string-full-nocuff/sldb-sv-pairwise-new-netprop")
+#sv3 = read.csv("/home/nikola/string-full-nocuff/sldb-sv-pairwise-new-netprop")
+sv3 = read.csv("sldb-sv-pairwise-new-netprop")
 sv3$gi = "SV"
-not = read.csv("/home/nikola/string-full-nocuff/not-pairwise-new-netprop") 
+#not = read.csv("/home/nikola/string-full-nocuff/not-pairwise-new-netprop") 
+not = read.csv("not-pairwise-new-netprop") 
 not$gi = "NOT"
 traindata = unique(rbind(sl,sl2,sl3,sv,sv2,sv3,not))
 table(traindata$gi)
@@ -23,7 +31,7 @@ traindatabal = DMwR::SMOTE(gi ~ ., traindata, perc.under = 200)
 table(traindatabal$gi)
 write.csv(traindatabal, "magical-combined-data.csv", row.names = F, quote = F)
 #training = traindatabal
-traindatabal = read.csv("/home/nikola/biogrid-new-ppi/magical-imp-prop-new-entrez/figures/magical-combined-data.csv")
+traindatabal = read.csv("magical-combined-data.csv")
 #traindatabal$gene1 = as.factor(traindatabal$gene1)
 #traindatabal$gene2 = as.factor(traindatabal$gene2)
 traindatabal$gi = as.factor(traindatabal$gi)
